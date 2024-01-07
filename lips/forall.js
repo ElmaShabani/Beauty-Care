42100e0ed4b0abf0c92f675a78cba98fa238879f
ronaatashollii <149106227+ronaatashollii@users.noreply.github.com>
2024-01-07 01:17:55 +0100

    update

--- lips/forall.js
+++ lips/forall.js
@@ -3,11 +3,7 @@ let cartData = JSON.parse(sessionStorage.getItem('cartData')) || { items: [], to
 function addToCart(productName, productPrice) {
     cartData.items.push({ name: productName, price: productPrice });
     cartData.total += productPrice;
-
-    // Save the updated cart data to sessionStorage
     sessionStorage.setItem('cartData', JSON.stringify(cartData));
-
-    // Update the UI immediately after adding to the cart
     updateCartUI();
 }
 
@@ -40,18 +36,13 @@ function toggleCart() {
     const cart = document.getElementById('cart');
     cart.classList.toggle('hidden');
 }
-
-// Listen for beforeunload event to clear the cart when the user leaves the website
 window.addEventListener('beforeunload', function () {
     sessionStorage.removeItem('cartData');
 });
 
-// Listen for page visibility change events to update the cart before leaving
 document.addEventListener('visibilitychange', function () {
     if (document.visibilityState === 'hidden') {
         sessionStorage.setItem('cartData', JSON.stringify(cartData));
     }
 });
-
-// Initial UI update when the page loads
 updateCartUI();
