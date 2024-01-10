fb4ca629fd37c159409dacbefc584f5e2fb2350a
ronaatashollii <149106227+ronaatashollii@users.noreply.github.com>
2024-01-10 21:27:22 +0100

    update

--- Projekti/jquery.js
+++ Projekti/jquery.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
     $("#shtype").click(function(){
-        $("#ann").css("color","brown").slideUp(3200);
+        $("#ann").css("color","blue").slideUp(3200);
         $("#text").text("Lips Section Sale at EverGlow Beauty.  ");
         $("#text").append("Go check it out!!!");
       });
